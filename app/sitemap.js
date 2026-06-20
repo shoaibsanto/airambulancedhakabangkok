7d365c71ba6bb8e540196f2aadd5613673848036
Shoaib Santo <santosarker00@gmail.com>
2026-06-20 03:43:20 +0000

    Daily SEO: add Dhaka to Bumrungrad insurance, payment & cost planning guide

--- app/sitemap.js
+++ app/sitemap.js
@@ -28,6 +28,7 @@ export default function sitemap() {
     ["/trauma-accident-evacuation", "weekly", 0.8],
     ["/insurance-coverage", "weekly", 0.7],
     ["/blog/icu-flight-bangladesh-to-bumrungrad-bangkok", "weekly", 0.7],
+    ["/blog/air-ambulance-insurance-payment-dhaka-bumrungrad", "weekly", 0.7],
     ["/privacy", "monthly", 0.3],
     ["/terms", "monthly", 0.3],
   ];
