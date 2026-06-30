5ee2bb38844b0fa1fac35603253a1eb118330243
Shoaib Santo <santosarker00@gmail.com>
2026-07-01 01:08:51 +0600

    SEO monthly 2026-07-01: Add dialysis page (new KW), expand thin blog, fix blog/index OG tags, sitemap +1

--- app/sitemap.js
+++ app/sitemap.js
@@ -43,6 +43,7 @@ export default async function sitemap() {
     ["/blog/neonatal-pediatric-air-ambulance-dhaka-bangkok", "weekly", 0.7],
     ["/blog/medical-visa-thailand-for-bangladeshi-patients", "weekly", 0.7],
     ["/blog/air-ambulance-dhaka-to-india-vellore-chennai-delhi", "weekly", 0.8],
+    ["/blog/dialysis-patient-air-ambulance-dhaka-bangkok", "weekly", 0.8],
   ];
 
   return ENTRIES.map(([urlPath, changeFrequency, priority]) => ({
